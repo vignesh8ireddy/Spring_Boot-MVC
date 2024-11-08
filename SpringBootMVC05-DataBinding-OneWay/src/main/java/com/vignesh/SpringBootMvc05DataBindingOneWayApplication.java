c80100907e3d444833a8ce671833373e7d68f875
Vignesh Reddy Ireddy <144072079+vignesh8reddy@users.noreply.github.com>
2024-11-07 20:40:37 -0600

    Commit by Vignesh

---
+++ SpringBootMVC05-DataBinding-OneWay/src/main/java/com/vignesh/SpringBootMvc05DataBindingOneWayApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvc05DataBindingOneWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvc05DataBindingOneWayApplication.class, args);
+	}
+
+}
